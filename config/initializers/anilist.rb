8a6d8bd34716e4a2db6875bd0993d4a57ddfbbd7
Liz Chhouk <kisaras.playground@gmail.com>
2017-05-01 15:34:42 -0400

    fix spelling error

--- config/initializers/anilist.rb
+++ config/initializers/anilist.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Kaiwa::Application.config.anilist_client_secret = ENV["ALINIST_CLIENT_SECRET"]
-Kaiwa::Application.config.anlist_client_id = ENV["ANILIST_CLIENT_ID"]
+Kaiwa::Application.config.anilist_client_secret = ENV["ANILIST_CLIENT_SECRET"]
+Kaiwa::Application.config.anilist_client_id = ENV["ANILIST_CLIENT_ID"]
