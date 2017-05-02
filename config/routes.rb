ec0f0fa5dbf50010f3f3bac25a5242cfc06f7ce1
Liz Chhouk <kisaras.playground@gmail.com>
2017-05-02 15:14:00 -0400

    set up controller for title-api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :anilistapi, only: [:index]
+      resources :titleapi, only: [:index]
     end
   end
 end
